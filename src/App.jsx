bae5db9ab6786d3c7668b2c7973b3e4da5db50c2
Amrkhwazir <amrkh5678@gmail.com>
2023-11-19 16:23:38 +0500

    v-tube project

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import styled, { ThemeProvider } from "styled-components"
+import Menu from "./components/Menu"
+import Navbar from "./components/Navbar";
+import { darkTheme, lightTheme } from "./utils/Theme";
+import { useState } from "react";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 6;
+  background-color: ${({theme}) => theme.bg};
+  color: ${({theme}) => theme.text}
+`;
+
+const Wrapper = styled.div`
+`;
+
+
+function App() {
+  
+  const [darkMode, setDarkMode] = useState(true)
+
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+
+    <Container>
+    <Menu setDarkMode={setDarkMode} darkMode={darkMode} />
+    <Main>
+      <Navbar />
+      <Wrapper>
+        Video Cards
+      </Wrapper>
+    </Main>
+    </Container>
+    
+    </ThemeProvider>
+  )
+}
+
+export default App
